6a59324b14d73bf81ef99aaaad6feaf6a0444439
Gonzalo-AG <gonzalonadresgalarza7@gmail.com>
2020-08-29 03:58:37 -0300

    Three commint

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          sipue mi gente
+          Conflicto
         </p>
         <a
           className="App-link"
