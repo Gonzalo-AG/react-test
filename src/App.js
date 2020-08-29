033f2adca81e3bf81b7e99e64c7e1b0bde3ba003
Agustin Fredes <agustinfredes@Agustins-MacBook-Pro.local>
2020-08-29 03:59:41 -0300

    third commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          sipue mi gente
+          conflicto 2s
         </p>
         <a
           className="App-link"
